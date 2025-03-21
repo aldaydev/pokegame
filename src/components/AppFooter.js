ef387609111b98e69930dfab56642ef7d99068b0
aldaydev <rafaldayparejo@gmail.com>
2025-03-21 17:30:21 +0100

    responsive footer

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -1,7 +1,19 @@
+import github_logo from '../assets/img/external/GitHub_Logo.png';
+import linkedIn_logo from '../assets/img/external/linkedIn_logo.png';
+import './AppFooter.css';
+
 const AppFooter = ()=>{
     return(
         <footer className="App-footer">
-            <p className="footer-text">RAFA ALDAY &copy; 2024</p>
+            <p className="footer-text">RAFAEL ALDAY &copy; 2024</p>
+            <div className="footer-links">
+                <a href='https://github.com/aldaydev' target='_blank' rel='noreferrer'>
+                    <img src={github_logo} alt='GitHub logo' className='footer-logo--github'/>
+                </a>
+                <a href='https://linkedin.com/in/rafaelalday' target='_blank' rel='noreferrer'>
+                    <img src={linkedIn_logo} alt='LinkedIn Logo' className='footer-logo--linkedIn'/>
+                </a>
+            </div>
         </footer>
     )
 }
